82eea5e03f309c2eaca21be0203221c58c92838f
michelelagreca <michelelagreca.work@gmail.com>
2023-04-18 19:00:00 +0200

    Created new branch with michele work: creation of the package and of the custome message definition

--- src/odom_node.cpp
+++ src/odom_node.cpp
@@ -1,42 +1,33 @@
-#include "ros/ros.h"
-#include "nav_msgs/Odometry.h"
-#include "first_project/Odom.h"
-#include <time.h>
+#include "ros/ros.h" //this is  a comment
 #include "std_msgs/String.h"
+#include <sstream> // to write somethingin the terminal
 
-#include <sstream>
-
-class odom{
-    nav_msgs::Odometry odom_message;
-    first_project::Odom cust_msg;
-
-
-private:
-    ros::NodeHandle n;
-
-    ros::Subscriber sub;
-    ros::Publisher pub;
-
-public:
-    odom(){
-        sub = n.subscribe("/steer_speed", 1000, &odom::callback, this);
-        pub = n.advertise<first_project::Odom>("/custom_odometry", 1000);  
-    }
-
-    void callback(const geometry_msgs::Quaternion& msg){
-        cust_msg.x = msg.x;
-        cust_msg.y = msg.y;
-        cust_msg.th = msg.w;
-        cust_msg.timestamp = std::to_string(ros::Time::now().toSec());   
-        pub.publish(cust_msg);
-        
-    }
-};
 
 int main(int argc, char **argv){
+    
+	ros::init(argc, argv, "odom_node"); //, ros::init_options::AnonymousName);
 
-    ros::init(argc, argv, "shuttle_odometry");
-    odom my_odom;
-    ros::spin();
-    return 0;
+	ros::NodeHandle n; // object that will be used to start the ros component
+
+	ros::Publisher odom_node = n.advertise<std_msgs::String>("odometry", 1000);
+
+	ros::Rate loop_rate(10);
+
+	int count = 0;
+
+  	while (ros::ok()){//same as TRUE, but ros ok will handle the shutdown of the nodes
+
+	    	std_msgs::String msg;
+    		std::stringstream ss;
+    		ss << "hello world " << count;
+
+    		msg.data = ss.str();
+    		ROS_INFO("%s", msg.data.c_str());
+    		chatter_pub.publish(msg);
+    		ros::spinOnce();
+    		loop_rate.sleep();
+    		++count;
+  	}
+  	return 0;
+
 }
